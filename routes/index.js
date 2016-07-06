0bd3887bb1fd0bc537c9a225211bcc3848019dce
elad <eladh87@gmail.com>
2016-07-06 22:56:32 +0300

    fix redirection

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.writeHead(301,
+    {Location: '/checkurl'}
+  );
+res.end();
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
